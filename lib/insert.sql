2882dd0addfcd79a37fa50b2c4ce227f7d6e4d82
Ranika Williams <rnkprice07@gmail.com>
2019-08-07 01:12:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr.Chocolate', 20, 'M', 'black', 'agressive', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 24, 'M', 'brown', 'nervous', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 36, 'F', 'black', 'agressive', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'brown', 'nervous', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'black', 'agressive', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'F', 'black', 'playful', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'brown', 'agressive', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 1, 'M', 'black', 'playful', 1);
